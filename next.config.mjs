0b7206247ff45044a17384addc691ae9bf99fdd6
Giorgi Gvimradze <natceo@gmail.com>
2024-05-22 13:36:42 +0400

    tsconfig update for module aliases NextJS

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+  experimental: {
+    taint: true,
+  },
+  // ...other config settings
+};
+
+export default nextConfig;
---
+++ src/app/(blog)/page.tsx
@@ -0,0 +1,14 @@
+import { SanityDocument } from "next-sanity";
+// import Posts from "@/components/Posts";
+
+import { sanityFetch } from "@/sanity/lib/fetch";
+import { POSTS_QUERY } from "@/sanity/lib/queries";
+import Posts from "@/components/Posts";
+
+export default async function Page() {
+  const posts = await sanityFetch<SanityDocument[]>({
+    query: POSTS_QUERY,
+  });
+
+  return <Posts posts={posts} />;
+}
